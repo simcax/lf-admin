6043e75ded697c78dd50a112ffc6a03aec74fc0c
Carsten Skov <carsten@simcax.dk>
2021-08-19 07:51:17 +0200

    Changed db connection to environment variable

--- app/db.py
+++ app/db.py
@@ -8,11 +8,12 @@ from os import environ
 
 def get_conn():
     if 'conn' not in g:
-        dbUser = environ.get("DB_USER")
-        dbPass = environ.get("DB_PASS")
-        dbHost = environ.get("DB_HOST","cockroachdb-public")
-        dbName = "lfadmin"
-        DATABASE_URL = "postgresql://{}@{}:26257/{}?sslmode=disable".format(dbUser, dbHost, dbName)
+        #dbUser = environ.get("DB_USER")
+        #dbPass = environ.get("DB_PASS")
+        #dbHost = environ.get("DB_HOST","cockroachdb-public")
+        #dbName = "lfadmin"
+        #DATABASE_URL = "postgresql://{}@{}:26257/{}?sslmode=disable".format(dbUser, dbHost, dbName)
+        DATABASE_URL = environ.get('DATABASE_URL')
         g.conn = psycopg2.connect(DATABASE_URL)
 
     return g.conn
